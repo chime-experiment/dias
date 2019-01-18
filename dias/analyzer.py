8a03786e8d2cd26f003eb2d350883d4c0a8d7ef9
Rick Nitsche <rick@phas.ubc.ca>
2019-01-17 18:01:51 -0800

    add sample analyzer, added logger, added config, added requirements.txt

--- dias/analyzer.py
+++ dias/analyzer.py
@@ -1,12 +1,12 @@
 # Analyzer Base Class
-# ---------------
+# -------------------
 
 import logging
+from caput import config
+from dias.utils.time_strings import str2timedelta, str2datetime
 
-# Set the module logger.
-log = logging.getLogger(__name__)
 
-class Task():
+class Analyzer(config.Reader):
     """Base class for all dias analyzers.
     All dias analyzers should inherit from this class, with functionality added
     by over-riding `setup`, `run` and/or `shutdown`.
@@ -19,10 +19,30 @@ class Task():
     ----------
     Methods
     -------
+    __init__
     setup
     run
     finish
     """
+
+    # Set the module logger.
+    logger = logging.getLogger(__name__)
+    FORMAT = '[%(asctime)s] %(name)s: %(message)s'
+    logging.basicConfig(format=FORMAT)
+    logger.setLevel(logging.INFO)
+
+
+    def __init__(self, name, write_dir):
+        """Constructor of analyzer base class.
+        """
+        self.name = name
+        self.write_dir = write_dir
+
+    start_time = config.Property(proptype=str2datetime)
+    period = config.Property(proptype=str2timedelta)
+
+    #prometheus = dias.Prometheus()
+
 
     # Overridable Attributes
     # -----------------------
@@ -30,18 +50,15 @@ class Task():
     def setup(self):
         """Initial setup stage of analyzer.
         """
-
         pass
 
     def finish(self):
         """Final clean-up stage of analyzer.
         """
-
         pass
 
     def run(self):
         """Main task stage of analyzer. Will be called by the dias framework
         according to the period set in the task config.
         """
-
         pass
