3612ab8c61084d18469ba3eb46cc8c38e2f03ef8
D. V. Wiebe <dvw@phas.ubc.ca>
2019-01-18 11:23:52 -0800

    Separate CHIME-specific analyzer features from base

--- dias/analyzer.py
+++ dias/analyzer.py
@@ -2,7 +2,6 @@
 # -------------------
 
 import logging
-from ch_util import data_index
 from caput import config
 from dias.utils.time_strings import str2timedelta, str2datetime
 
@@ -40,14 +39,6 @@ class Analyzer(config.Reader):
 
     start_time = config.Property(proptype=str2datetime)
     period = config.Property(proptype=str2timedelta)
-    archive_data_dir = config.Property(proptype=str)
-
-    def Finder(self, acqs=()):
-        """Returns a ch_util Finder object for use by Analyzer tasks
-        """
-        
-        return data_index.Finder(acqs=acqs,
-                node_spoof= { "gong" : archive_data_dir })
 
     #prometheus = dias.Prometheus()
 
