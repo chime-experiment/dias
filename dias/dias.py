5f2dd000387f0a92f4345437cfca779ec7a0825c
D. V. Wiebe <dvw@phas.ubc.ca>
2019-01-17 17:21:12 -0800

    Better error reporting

--- dias/dias.py
+++ dias/dias.py
@@ -21,25 +21,27 @@ def import_analyzer_class(name):
     # Split the name into a module and a classname
     (modulename, separator, classname) = name.rpartition('.')
 
-    try:
-        # Check if we successfully split
-        if separator == "":
-            # No module, look for name in the global list
-            # Throws KeyError if not found
+    # Check if we successfully split
+    if separator == "":
+        # No module, look for name in globals list
+        try:
             clas = globals()[classname]
-        else:
-            # Load the module.  Throws ImportError on error
+        except KeyError:
+            raise ImportError("Analyzer class {0} not found".format(classname))
+    else:
+        # Try to load the module
+        try:
             ext_module = importlib.import_module(modulename)
+        except ImportError:
+            raise ImportError("Analyzer module {0} not found".format(
+                modulename))
 
-            # Find the class in the module.  Throws AttributeError on error
+        # Now, find the class in the module
+        try:
             clas = getattr(ext_module, classname)
-    except ImportError:
-        raise ImportError("Analyzer module {0} not found".format(module_path))
-    except KeyError:
-        raise ImportError("Analyzer class {0} not found".format(classname))
-    except AttributeError:
-        raise ImportError("Analyzer class {0} not found in module {1}".format(
-            modulename, classname))
+        except AttributeError:
+            raise ImportError(
+                    "Analyzer class {0} not found in module {1}".format(
+                        classname, modulename))
 
     return clas
- 
