c6fbcfa956131442f2caf334bf925b8854239bf9
wulfda02 <wulfda02@luther.edu>
2019-01-17 18:00:02 -0800

    Update dias.py

--- dias/dias.py
+++ dias/dias.py
@@ -18,17 +18,27 @@ class config_task(config.Reader):
     analyzer = config.Property(proptype=str, key='analyzer')
     
 def load_analyzers():
+    """
+    Locate and load all task config files
+    """
+    
+    # caput config reader class for global config
     global_config = config_global()
     global_file = open("dias.conf","r")
     global_config.read_config(yaml.load(global_file))
     global_file.close()
+    
     config_dir = global_config.task_config_dir
     for config_file in os.listdir(config_dir):
+        
         if config_file.endswith("ad.conf"):
+            
             task_file = open(os.path.join(config_dir, config_file),"r")
+            
+            # caput config reader class for task config
             task_config = config_task()
             task_config.read_config(yaml.load(task_file))
-            print(task_config.analyzer)
+    
 
 def import_analyzer_class(name):
     """
