05c45321f9e07adadfe4709c6ebe3a1553596e2d
D. V. Wiebe <dvw@phas.ubc.ca>
2019-01-17 17:14:55 -0800

    import_analyzer_class() to import a thing

--- dias/dias.py
+++ dias/dias.py
@@ -1,3 +1,4 @@
+import importlib
 import logging
 
 # Set the module logger.
@@ -8,3 +9,37 @@ def run_tasks():
 
 def load_analyzers():
     pass
+
+def import_analyzer_class(name):
+    """
+    Imports the Analyser class given by name.  If name includes a module,
+    first imports the module.
+
+    Returns the imported class.
+    """
+
+    # Split the name into a module and a classname
+    (modulename, separator, classname) = name.rpartition('.')
+
+    try:
+        # Check if we successfully split
+        if separator == "":
+            # No module, look for name in the global list
+            # Throws KeyError if not found
+            clas = globals()[classname]
+        else:
+            # Load the module.  Throws ImportError on error
+            ext_module = importlib.import_module(modulename)
+
+            # Find the class in the module.  Throws AttributeError on error
+            clas = getattr(ext_module, classname)
+    except ImportError:
+        raise ImportError("Analyzer module {0} not found".format(module_path))
+    except KeyError:
+        raise ImportError("Analyzer class {0} not found".format(classname))
+    except AttributeError:
+        raise ImportError("Analyzer class {0} not found in module {1}".format(
+            modulename, classname))
+
+    return clas
+ 
