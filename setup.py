139d6966e63dd55cea3076680872403cfaf068d1
Don Wiebe <dvw@phas.ubc.ca>
2019-01-18 12:05:10 -0800

    One more subpackage to install

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author           = "The CHIME Collaboration",
     author_email     = "dvw@phas.ubc.ca",
     description      = "CHIME data integrity automation system",
-    packages         = ['dias','dias.analyzers'],
+    packages         = ['dias','dias.analyzers', 'dias.utils'],
     requires         = ['caput', 'ch_util'],
     license          = "GPL v3.0",
     url              = "http://github.com/chime-experiment/dias"
