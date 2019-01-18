648722d70a60c67d374bb2dbf737d98e4cb06a26
Rick Nitsche <rick@phas.ubc.ca>
2019-01-18 14:08:06 -0800

    create task write directories, ignore task files with leading _, add log_level config var

--- dias/analyzer.py
+++ dias/analyzer.py
@@ -4,10 +4,6 @@
 import logging
 from caput import config
 from dias.utils.time_strings import str2timedelta, str2datetime
-from dias import prometheus
-
-# This is how a log line produced by analyzers will look:
-LOG_FORMAT = '[%(asctime)s] %(name)s: %(message)s'
 
 class Analyzer(config.Reader):
     """Base class for all dias analyzers.
@@ -28,15 +24,17 @@ class Analyzer(config.Reader):
     finish
     """
 
-    def __init__(self, name, write_dir):
+    def __init__(self, name, write_dir, prometheus):
         """Constructor of analyzer base class.
         """
         self.name = name
         self.write_dir = write_dir
+
         # Set the module logger.
         self.logger = logging.getLogger(name)
         self.logger.setLevel(logging.INFO)
-        logging.basicConfig(format=LOG_FORMAT)
+
+        self._prometheus = prometheus
 
     start_time = config.Property(proptype=str2datetime)
     period = config.Property(proptype=str2timedelta)
@@ -48,8 +46,8 @@ class Analyzer(config.Reader):
         `dias_task_<task name>_<metric_name>`."""
         labels['analyzer'] = __name__
         metric_name = 'dias_task_' + self.name + '_' + metric_name
-        prometheus._add_metric(metric_name, value, documentation,
-                               timestamp=None, labels=labels, unit=unit)
+        self._prometheus.add_metric(metric_name, value, documentation,
+                                    timestamp=None, labels=labels, unit=unit)
 
     def data_metric(self, metric_name, value, documentation=None,
                     labels=dict(), unit=''):
@@ -60,8 +58,8 @@ class Analyzer(config.Reader):
         labels['task'] = self.name
         labels['analyzer'] = __name__
         metric_name = 'dias_data_' + self.name + '_' + metric_name
-        prometheus._add_metric(metric_name, value, documentation,
-                               timestamp=None, labels=labels, unit=unit)
+        self._prometheus.add_metric(metric_name, value, documentation,
+                                     timestamp=None, labels=labels, unit=unit)
 
 
     # Overridable Attributes
--- dias/service.py
+++ dias/service.py
@@ -9,9 +9,8 @@ import yaml
 import os
 from dias import prometheus
 
-# Set the module logger.
-logger = logging.getLogger(__name__)
-
+# This is how a log line produced by dias will look:
+LOG_FORMAT = '[%(asctime)s] %(name)s: %(message)s'
 
 class service(config.Reader):
 
@@ -20,19 +19,29 @@ class service(config.Reader):
                                       proptype=str, key='task_config_dir')
     task_write_dir = config.Property(default='/tmp/', proptype=str)
     prometheus_client_port = config.Property(default=4444, proptype=int)
+    log_level = config.Property(default='INFO', proptype=logging.getLevelName)
 
     def __init__(self, config_path):
         self.config_path = config_path
 
         self.tasks = list()
 
-        self.load_analyzers()
+        # Read and apply dias global config
+        global_file = open(self.config_path, "r")
+        self.read_config(yaml.load(global_file))
+        global_file.close()
 
-        # Setup tasks
-        self.setup_tasks()
+        # Set the module logger.
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(self.log_level)
+        logging.basicConfig(format=LOG_FORMAT)
 
         # Start prometheus client
         self.prometheus = prometheus.Prometheus(self.prometheus_client_port)
+
+        # Setup tasks
+        self.load_analyzers()
+        self.setup_tasks()
 
     def setup_tasks(self):
         for task in self.tasks:
@@ -40,20 +49,17 @@ class service(config.Reader):
 
     def run_tasks(self):
         pass
-    
+
     def load_analyzers(self):
         """
         Locate and load all task config files
         """
 
-        # caput config reader class for global config
-        global_file = open(self.config_path, "r")
-        self.read_config(yaml.load(global_file))
-        global_file.close()
-
         for config_file in os.listdir(self.task_config_dir):
-
-            if config_file.endswith(".conf"):
+            # Only accept files ending in .conf as task configs.
+            # Task config files starting with an underscore (_) are disabled.
+            if config_file.endswith(".conf") and not\
+                    config_file.startswith("_"):
 
                 # caput config reader class for task config
                 task_file = open(os.path.join(self.task_config_dir,
@@ -63,15 +69,29 @@ class service(config.Reader):
                 # Load the analyzer for this task from the task config
                 analyzer_class = self.import_analyzer_class(task_config['analyzer'])
 
+                # Remove .conf from the config file name to get the name of the
+                # task
                 task_name = config_file[:-5]
-                write_dir = task_name + '/' + task_name
 
-                task = analyzer_class(task_name, write_dir)
+                # This is where we tell the task to write its output
+                write_dir = self.task_write_dir + '/' + task_name
+
+                # Create the directory if it doesn't exist
+                if not os.path.isdir(write_dir):
+                    self.logger.info('Creating new write directory for task `'
+                                      + task_name + '`: ' + write_dir)
+                    os.makedirs(write_dir)
+                else:
+                    self.logger.info('Set write directory for task `'
+                                     + task_name + '` to existing path: ' +
+                                     write_dir)
+
+                task = analyzer_class(task_name, write_dir, self.prometheus)
                 task.read_config(task_config)
                 self.tasks.append(task)
 
                 task_file.close()
-    
+
 
     def import_analyzer_class(self, name):
         """
