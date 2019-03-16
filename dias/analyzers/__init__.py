abe0e192e6e244694cc296bff046257f734e4578
Rick Nitsche <rick@phas.ubc.ca>
2019-03-15 21:11:38 -0700

    import all analyzer classes in the analyzers module

---
+++ dias/analyzers/__init__.py
@@ -0,0 +1,4 @@
+from .feedpositions_analyzer import FeedpositionsAnalyzer
+from .sample_analyzer import SampleAnalyzer
+from .test_analyzer import TestAnalyzer
+from .thermaldata_analyzer import ThermalDataAnalyzer
