2bd8f24c206452825b6d1c53ef295e9fb32a1c86
anjakefala <anja.kefala@gmail.com>
2019-11-25 14:30:49 -0800

    feat: file_list is now a list of files

--- dias/analyzers/sensitivity_analyzer.py
+++ dias/analyzers/sensitivity_analyzer.py
@@ -190,11 +190,8 @@ class SensitivityAnalyzer(CHIMEAnalyzer):
             instrument=self.instrument, data_product=self.acq_suffix
         )
         file_list = self.filter_files_by_time(file_list, start_time, stop_time)
-        try:
-            all_files = file_list[0][0]
-            if not all_files:
-                raise IndexError()
-        except IndexError:
+
+        if not file_list:
             err_msg = "No {}_{} files found from last {}.".format(
                 self.instrument, self.acq_suffix, self.period
             )
@@ -215,7 +212,7 @@ class SensitivityAnalyzer(CHIMEAnalyzer):
         )
 
         # Read a sample file for getting index map
-        file_sample = all_files[0]
+        file_sample = file_list[0]
         data = andata.CorrData.from_acq_h5(
             file_sample,
             datasets=["reverse_map", "flags/inputs"],
@@ -228,7 +225,7 @@ class SensitivityAnalyzer(CHIMEAnalyzer):
             data.index_map, inputmap
         )
 
-        for files in all_files:
+        for files in file_list:
 
             # Load index map and reverse map
             data = andata.CorrData.from_acq_h5(
