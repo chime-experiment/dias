d674e08b25dc12b86b2b55492bd7d2b451769d83
anjakefala <anja.kefala@gmail.com>
2019-11-29 16:43:23 -0800

    feat: move temp data loading to its own function. start work on psd.

--- dias/analyzers/rx_temp_analyzer.py
+++ dias/analyzers/rx_temp_analyzer.py
@@ -1,9 +1,12 @@
 import datetime
 import requests
+import numpy as np
 
 from caput import config
 from dias import chime_analyzer
 from dias.utils.string_converter import str2timedelta
+import matplotlib as plt
+from psd import PowerSpectralDensity
 
 __version__ = "0.1.0"
 
@@ -24,10 +27,12 @@ class RxTempAnalyzer(chime_analyzer.CHIMEAnalyzer):
             "Starting up. My name is %s and I am of type %s." % (self.name, __name__)
         )
 
-    def run(self):
-        """Run the analyzer.
-        """
+    def load_temp(self):
         step = 30.0
+        output = [dict(), dict()]
+        for ind in range(2):
+            output[ind]["time"] = []
+            output[ind]["temp"] = []
 
         # Determine the range of time to process
         end_datetime = datetime.datetime.utcnow()
@@ -37,18 +42,44 @@ class RxTempAnalyzer(chime_analyzer.CHIMEAnalyzer):
         ets = end_datetime.strftime("%Y-%m-%dT%H:%M:%SZ")
 
         # Query prometheus running on hk-east
-        prom_query = 'ext_sensor_value{device="Ambient Temperature", type="temperature", job="environment_sensors", instance="e-enviromux"}/10'
+        prom_query_east = 'ext_sensor_value{device="Ambient Temperature", type="temperature", job="environment_sensors", instance="e-enviromux"}/10'
+        prom_query_west = 'ext_sensor_value{device="Ambient Temperature", type="temperature", job="environment_sensors", instance="w-enviromux"}/10'
 
-        http_query = (
-            "http://hk-east:9090/api/v1/query_range?query=%s&start="
-            "%s&end=%s&step=%dm" % (prom_query, sts, ets, step)
-        )
+        for ind, prom_query in enumerate([prom_query_east, prom_query_west]):
 
-        resp = requests.get(http_query)
-        jresp = resp.json()
+            http_query = (
+                "http://hk-east:9090/api/v1/query_range?query=%s&start="
+                "%s&end=%s&step=%dm" % (prom_query, sts, ets, step)
+            )
 
-        for res in jresp["data"]["result"]:
-            print(res)
+            resp = requests.get(http_query)
+            jresp = resp.json()
+
+            for res in jresp["data"]["result"]:
+                timestamp, temp = zip(*res["values"])
+                output[ind]["time"] += timestamp
+                output[ind]["temp"] += temp
+
+            output[ind]["time"] = np.array(output[ind]["time"])
+            output[ind]["temp"] = np.array(output[ind]["temp"])
+
+        report = {}
+        report["time"] = output[0]["time"]
+        report["temp"] = np.vstack([output[0]["temp"], output[1]["temp"]])
+
+        return report
+
+
+    def run(self):
+        """Run the analyzer.
+        """
+        report = self.load_temp()
+
+        timestamp = report["time"]
+        temp = report["temp"]
+
+        temp_psd = PowerSpectralDensity(timestamp, temp, overlap_factor=0.5, tmax=0.1*(timestamp[-1] - timestamp[0]), speedy=True, one_sided=True)
+
 
     def finish(self):
         """Close connections."""
