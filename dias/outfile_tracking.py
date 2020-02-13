cd520a053880271e92dfcef7c31739926dd8182a
Rick Nitsche <rick@phas.ubc.ca>
2020-02-13 14:57:06 -0800

    refactor(outfile_tracking): move into its own module
    Closes #91

--- dias/outfile_tracking.py
+++ dias/outfile_tracking.py
@@ -1,10 +1,11 @@
-import ephemeris
-import numpy as np
+"""Track output files and processed times."""
+
+import copy
+from ch_util import ephemeris
 import os
 import sqlite3
-import copy
 
-from .exception import DiasException
+from dias.exception import DiasException
 
 
 class FileTracker:
@@ -45,7 +46,9 @@ class FileTracker:
         self._additional_vars = kwargs
 
         # prepare create and insert commands for additional variables
-        self._create_cmd = "CREATE TABLE IF NOT EXISTS files(start TIMESTAMP, stop TIMESTAMP, "
+        self._create_cmd = (
+            "CREATE TABLE IF NOT EXISTS files(start TIMESTAMP, stop TIMESTAMP, "
+        )
         self._insert_cmd = "INSERT INTO files VALUES (?, ?, "
         for name, default in self._additional_vars.items():
             if type(default) is int:
@@ -53,11 +56,13 @@ class FileTracker:
             else:
                 raise DiasException(
                     "Type of additional value {}: {} is not supported (yet).".format(
-                        name, type(default)))
+                        name, type(default)
+                    )
+                )
             self._create_cmd += "{} {}, ".format(name, typename)
             self._insert_cmd += "?, "
         self._create_cmd += "filename TEXT UNIQUE ON CONFLICT REPLACE)"
-        self._insert_cmd += + "?)"
+        self._insert_cmd += "?)"
 
         # Open connection to data index database
         # and create table if it does not exist.
@@ -79,9 +84,21 @@ class FileTracker:
     def __del__(self):
         self.data_index.close()
 
-    def get_start_time(self):
-        # Refresh the database
-        self._refresh_data_index()
+    def get_start_time(self, refresh_db=True):
+        """
+        Get the next start time.
+
+        Parameters
+        ----------
+        refresh_db : bool
+            Refresh the database before getting the start time (default: True).
+
+        Returns
+        -------
+        UTC date and time of next start time.
+        """
+        if refresh_db:
+            self._refresh_data_index()
         cursor = self.data_index.cursor()
         query = "SELECT stop FROM files ORDER BY stop DESC LIMIT 1"
         results = list(cursor.execute(query))
@@ -116,7 +133,9 @@ class FileTracker:
         # check additional variables
         for k in kwargs:
             if k not in self._additional_vars.keys():
-                raise DiasException("Found unexpected key in additional variables: {}".format(k))
+                raise DiasException(
+                    "Found unexpected key in additional variables: {}".format(k)
+                )
         # start with default values and then update given additional values
         additional_vars = copy.copy(self._additional_vars)
         additional_vars.update(kwargs)
@@ -131,9 +150,7 @@ class FileTracker:
 
         # Insert row for this file
         cursor = self.data_index.cursor()
-        cursor.execute(
-            self._insert_command, (dt_start, dt_stop, **kwargs, relpath)
-        )
+        cursor.execute(self._insert_command, (dt_start, dt_stop, kwargs, relpath))
 
         self.data_index.commit()
         self.logger.info("Added %s to data index database." % relpath)
