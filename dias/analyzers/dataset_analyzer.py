420f9ba9120e8b673b500512ba16c4fe111a549c
Rick Nitsche <rick@phas.ubc.ca>
2020-02-14 10:41:51 -0800

    fix dataset analyzer

--- dias/analyzers/dataset_analyzer.py
+++ dias/analyzers/dataset_analyzer.py
@@ -1,6 +1,6 @@
 from dias import CHIMEAnalyzer
 from dias.outfile_tracking import FileTracker
-from dias.utils.string_converter import datetime2str
+from dias.utils.string_converter import datetime2str,str2timedelta
 from dias.exception import DiasDataError
 
 from caput import config
@@ -13,80 +13,15 @@ import datetime
 import numpy as np
 
 
-def validate_flag_updates(ad, flg, freq_id):
-
-    # fmt: off
-    extra_bad = [
-      # These are non-CHIME feeds we want to exclude (26m, noise source channels, etc.)
-        46,  142,  688,  944,  960, 1058, 1166, 1225, 1314, 1521, 2032, 2034,
-      # Below are the last eight feeds on each cylinder, masked out because their beams are very
-      # different
-         0,    1,    2,    3,    4,    5,    6,    7,  248,  249,  250,  251,  252,  253,  254,  255,
-       256,  257,  258,  259,  260,  261,  262,  263,  504,  505,  506,  507,  508,  509,  510,  511,
-       512,  513,  514,  515,  516,  517,  518,  519,  760,  761,  762,  763,  764,  765,  766,  767,
-       768,  769,  770,  771,  772,  773,  774,  775, 1016, 1017, 1018, 1019, 1020, 1021, 1022, 1023,
-      1024, 1025, 1026, 1027, 1028, 1029, 1030, 1031, 1272, 1273, 1274, 1275, 1276, 1277, 1278, 1279,
-      1280, 1281, 1282, 1283, 1284, 1285, 1286, 1287, 1528, 1529, 1530, 1531, 1532, 1533, 1534, 1535,
-      1536, 1537, 1538, 1539, 1540, 1541, 1542, 1543, 1784, 1785, 1786, 1787, 1788, 1789, 1790, 1791,
-      1792, 1793, 1794, 1795, 1796, 1797, 1798, 1799, 2040, 2041, 2042, 2043, 2044, 2045, 2046, 2047
-    ]
-    # fmt: on
-
-    # Get the unique dataset_ids in each file
-    file_ds = ad.flags["dataset_id"][:]
-    unique_ds = np.unique(file_ds)
-
-    # Find the flagging update_id for each dataset
-    states = {
-        id: (
-            ds.Dataset.from_id(bytes(id).decode())
-            .closest_ancestor_of_type("flags")
-            .dataset_state.data["data"]
-            .encode()
-        )
-        for id in unique_ds
-    }
-
-    ret = {}
-
-    for ds_id, update_id in states.items():
-
-        # Get all non-missing frames at this frequency
-        present = ad.flags["frac_lost"][freq_id] < 1.0
-
-        # Select all present frames with the dataset id we want
-        sel = (file_ds[freq_id] == ds_id) & present
-
-        # Extract the input flags and mark any extra missing data
-        # (i.e. that static mask list in baselineCompression)
-        flags = ad.flags["inputs"][:, sel].astype(np.bool)
-        flags[extra_bad, :] = False
-
-        # Find the flag update from the files
-        flagsfile = None
-        for f in flg:
-            update_ids = list(f.update_id)
-            dsid = states[ds_id]
-            try:
-                flgind = update_ids.index(dsid)
-            except ValueError as err:
-                print("Flags not found in file f: {}".format(err))
-                continue
-            flagsfile = f.flag[flgind]
-        if flagsfile is None:
-            raise DiasDataError("Flags for file {} not found.".format(ad))
-        flagsfile[extra_bad] = False
-
-        # Test if all flag entries match the one from the flaginput file
-        ret[ds_id] = (flags == flagsfile[:, np.newaxis]).all()
-
-    return ret
-
-
 class DatasetAnalyzer(CHIMEAnalyzer):
+    """
+    offset : str
+        Process data this timedelta before current time.
+    """
     instrument = config.Property(proptype=str, default="chimestack")
     flags_instrument = config.Property(proptype=str, default="chime")
     freq_id = config.Property(proptype=int, default=10)
+    offset = config.Property(proptype=str2timedelta, default="2h")
 
     def setup(self):
         self.tracker = FileTracker(self)
@@ -161,4 +96,80 @@ class DatasetAnalyzer(CHIMEAnalyzer):
                     for all_flag_files, (flag_tstart, flag_tend) in flag_acq_results:
                         flg.append(andata.FlagInputData.from_acq_h5(all_flag_files))
 
-                    self.logger.info(validate_flag_updates(ad, flg, self.freq_id))
+                    self.logger.info(self.validate_flag_updates(ad, flg))
+
+    def validate_flag_updates(self, ad, flg):
+
+        # fmt: off
+        extra_bad = [
+            # These are non-CHIME feeds we want to exclude (26m, noise source channels, etc.)
+            46, 142, 688, 944, 960, 1058, 1166, 1225, 1314, 1521, 2032, 2034,
+            # Below are the last eight feeds on each cylinder, masked out because their beams are very
+            # different
+            0, 1, 2, 3, 4, 5, 6, 7, 248, 249, 250, 251, 252, 253, 254, 255,
+            256, 257, 258, 259, 260, 261, 262, 263, 504, 505, 506, 507, 508, 509, 510,
+            511,
+            512, 513, 514, 515, 516, 517, 518, 519, 760, 761, 762, 763, 764, 765, 766,
+            767,
+            768, 769, 770, 771, 772, 773, 774, 775, 1016, 1017, 1018, 1019, 1020, 1021,
+            1022, 1023,
+            1024, 1025, 1026, 1027, 1028, 1029, 1030, 1031, 1272, 1273, 1274, 1275,
+            1276, 1277, 1278, 1279,
+            1280, 1281, 1282, 1283, 1284, 1285, 1286, 1287, 1528, 1529, 1530, 1531,
+            1532, 1533, 1534, 1535,
+            1536, 1537, 1538, 1539, 1540, 1541, 1542, 1543, 1784, 1785, 1786, 1787,
+            1788, 1789, 1790, 1791,
+            1792, 1793, 1794, 1795, 1796, 1797, 1798, 1799, 2040, 2041, 2042, 2043,
+            2044, 2045, 2046, 2047
+        ]
+        # fmt: on
+
+        # Get the unique dataset_ids in each file
+        file_ds = ad.flags["dataset_id"][:]
+        unique_ds = np.unique(file_ds)
+
+        # Find the flagging update_id for each dataset
+        states = {
+            id: (
+                ds.Dataset.from_id(bytes(id).decode())
+                    .closest_ancestor_of_type("flags")
+                    .dataset_state.data["data"]
+                    .encode()
+            )
+            for id in unique_ds
+        }
+
+        ret = {}
+
+        for ds_id, update_id in states.items():
+
+            # Get all non-missing frames at this frequency
+            present = ad.flags["frac_lost"][self.freq_id] < 1.0
+
+            # Select all present frames with the dataset id we want
+            sel = (file_ds[self.freq_id] == ds_id) & present
+
+            # Extract the input flags and mark any extra missing data
+            # (i.e. that static mask list in baselineCompression)
+            flags = ad.flags["inputs"][:, sel].astype(np.bool)
+            flags[extra_bad, :] = False
+
+            # Find the flag update from the files
+            flagsfile = None
+            for f in flg:
+                update_ids = list(f.update_id)
+                dsid = states[ds_id]
+                try:
+                    flgind = update_ids.index(dsid)
+                except ValueError as err:
+                    print("Flags not found in file f: {}".format(err))
+                    continue
+                flagsfile = f.flag[flgind]
+            if flagsfile is None:
+                raise DiasDataError("Flags for file {} not found.".format(ad))
+            flagsfile[extra_bad] = False
+
+            # Test if all flag entries match the one from the flaginput file
+            ret[ds_id] = (flags == flagsfile[:, np.newaxis]).all()
+
+        return ret
