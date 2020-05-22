c68eaea0079916bf78dce9bae456169005cb8caa
anjakefala <anja.kefala@gmail.com>
2020-05-22 13:30:17 -0700

    .

--- dias/analyzers/source_spectra_analyzer.py
+++ dias/analyzers/source_spectra_analyzer.py
@@ -1,4 +1,4 @@
-"""Extracts flux of a given source at zenith, as a function of freq.
+"""Extracts flux of a given source at zenith. Does this as a function of freq.
 
 .. currentmodule:: dias.analyzers.source_spectra_analyzer
 
@@ -92,7 +92,7 @@ class SourceSpectraAnalyzer(CHIMEAnalyzer):
     ha
         1D array which contains the hour angle covered in the output file.
     param
-        parameter names describing fit to the transit 
+        parameter names describing fit to the transit
 
 
     Datasets
@@ -155,7 +155,7 @@ class SourceSpectraAnalyzer(CHIMEAnalyzer):
     Attributes
     ----------
     offset : timedelta
-       A time period (e.g. '1h'), before time of script execution for searching the files (default `4h`). 
+       A time period (e.g. '1h'), before time of script execution for searching the files (default `4h`).
     correlator : str
         Source of the input data
     acq_suffix : str
